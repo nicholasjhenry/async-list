c85412c26101f2d4bf7d4d79cf3be140296aebb8
Nicholas Henry <nicholas.henry@gmail.com>
2018-12-29 11:02:30 -0500

    Add domain types

--- lib/async_list.ex
+++ lib/async_list.ex
@@ -1,2 +1,27 @@
 defmodule AsyncList do
+  defmodule SystemUri do
+    use TypedStruct
+
+    typedstruct do
+      field :value, String.t, enforce: true
+    end
+  end
+
+  defmodule UriContent do
+    # type UriContent = UriContent of System.Uri * string
+    use TypedStruct
+
+    typedstruct do
+      field :value, {SystemURI, String.t}, enforce: true
+    end
+  end
+
+  defmodule UriContentSize do
+    # type UriContentSize = UriContentSize of System.Uri * int
+    use TypedStruct
+
+    typedstruct do
+      field :value, {SystemURI, non_neg_integer}, enforce: true
+    end
+  end
 end
