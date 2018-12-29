2cc3911fd94bf45d2508576d242757ac78ef664a
Nicholas Henry <nicholas.henry@gmail.com>
2018-12-29 08:37:17 -0500

    Remove generated code

--- lib/async_list.ex
+++ lib/async_list.ex
@@ -1,18 +1,2 @@
 defmodule AsyncList do
-  @moduledoc """
-  Documentation for AsyncList.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> AsyncList.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/async_list_test.exs
+++ test/async_list_test.exs
@@ -1,8 +1,3 @@
 defmodule AsyncListTest do
   use ExUnit.Case
-  doctest AsyncList
-
-  test "greets the world" do
-    assert AsyncList.hello() == :world
-  end
 end
