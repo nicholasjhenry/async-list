cb74489a1cb4502a979ef7fb4e816617ab6aef02
Nicholas Henry <nicholas.henry@gmail.com>
2018-12-29 08:20:05 -0500

    🌱

---
+++ lib/async_list.ex
@@ -0,0 +1,18 @@
+defmodule AsyncList do
+  @moduledoc """
+  Documentation for AsyncList.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AsyncList.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/async_list_test.exs
@@ -0,0 +1,8 @@
+defmodule AsyncListTest do
+  use ExUnit.Case
+  doctest AsyncList
+
+  test "greets the world" do
+    assert AsyncList.hello() == :world
+  end
+end
